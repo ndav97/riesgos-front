3dc10a4bc81c5e9bf10a5ae284b228592532220a
ndav97 <nicolasdav97@gmail.com>
2020-08-29 16:44:44 -0500

    asdas

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const serveStatic = require('serve-static')
-const path = require('path')
+const history = require('connect-history-api-fallback')
 const app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(history())
+app.use(serveStatic(__dirname.join('/dist/spa')))
 const port = process.env.PORT || 80
 app.listen(port)
